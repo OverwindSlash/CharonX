c9996b7f1409e5ab71c4a17380245a1e75e3f412
GreatWaller <zhangxuan4422@live.cn>
2020-04-29 15:23:51 +0800

    Revert "Revert "dynamic add feature and permission""
    This reverts commit 7f2812ea4f5c03ae7e0d3f442835b90878cf8183.

---
+++ src/CharonX.Application/Permissions/PermissionSettingAppService.cs
@@ -0,0 +1,124 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Authorization;
+using Abp.AutoMapper;
+using Abp.Configuration;
+using Abp.Domain.Repositories;
+using CharonX.Entities;
+using CharonX.Permissions.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharonX.Permissions
+{
+    /// <summary>
+    /// 自定义权限
+    /// </summary>
+    public class PermissionSettingAppService : ApplicationService, IPermissionSettingAppService
+    {
+        private readonly IRepository<CustomPermissionSetting> permissionRepository;
+        private readonly IRepository<CustomFeatureSetting> featureRepository;
+
+        public PermissionSettingAppService(IRepository<CustomPermissionSetting> permissionRepository,
+            IRepository<CustomFeatureSetting> featureRepository)
+        {
+            this.permissionRepository = permissionRepository;
+            this.featureRepository = featureRepository;
+        }
+        /// <summary>
+        /// 新建自定义权限
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomPermissionSettingDto> CreatePermission(CustomPermissionSettingDto input)
+        {
+            var setting = await permissionRepository.InsertAsync(ObjectMapper.Map<CustomPermissionSetting>(input));
+            return ObjectMapper.Map<CustomPermissionSettingDto>(setting);
+        }
+        /// <summary>
+        /// 获取单个权限
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomPermissionSettingDto> GetPermission(EntityDto input)
+        {
+            var setting = await permissionRepository.FirstOrDefaultAsync(p => p.Id == input.Id);
+            if (setting==null)
+            {
+                return new CustomPermissionSettingDto();
+            }
+            return ObjectMapper.Map<CustomPermissionSettingDto>(setting);
+        }
+        /// <summary>
+        /// 获取全部权限
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<CustomPermissionSettingDto>> GetAllPermissions()
+        {
+            var settings = await permissionRepository.GetAllListAsync();
+            return ObjectMapper.Map<List<CustomPermissionSettingDto>>(settings);
+        }
+        /// <summary>
+        /// 更新指定权限
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomPermissionSettingDto> UpdatePermission(CustomPermissionSettingDto input)
+        {
+            var setting= await permissionRepository.FirstOrDefaultAsync(p => p.Id == input.Id);
+            ObjectMapper.Map(input,setting);
+            return input;
+        }
+        /// <summary>
+        /// 自定义功能包
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomFeatureSettingDto> CreateFeature(CustomFeatureSettingDto input)
+        {
+            var setting = await featureRepository.InsertAsync(ObjectMapper.Map<CustomFeatureSetting>(input));
+            return ObjectMapper.Map<CustomFeatureSettingDto>(setting);
+        }
+        /// <summary>
+        /// 更新指定功能包
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomFeatureSettingDto> UpdateFeature(CustomFeatureSettingDto input)
+        {
+            var setting = await featureRepository.FirstOrDefaultAsync(p => p.Id == input.Id);
+            ObjectMapper.Map(input, setting);
+            return input;
+        }
+        /// <summary>
+        /// 获取指定功能包
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<CustomFeatureSettingDto> GetFeature(EntityDto input)
+        {
+            var setting = await featureRepository.FirstOrDefaultAsync(p => p.Id == input.Id);
+            if (setting == null)
+            {
+                return new CustomFeatureSettingDto();
+            }
+            return ObjectMapper.Map<CustomFeatureSettingDto>(setting);
+        }
+        /// <summary>
+        /// 获取全部功能包
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<CustomFeatureSettingDto>> GetAllFeatures()
+        {
+            var settings = await featureRepository.GetAllListAsync();
+            return ObjectMapper.Map<List<CustomFeatureSettingDto>>(settings);
+        }
+
+        //private Task CreateOrUpdateXmlNode()
+        //{
+        //    XDocument xmlFile=XDocument.Load()
+        //}
+    }
+}
--- src/CharonX.Core/Features/PesCloudFeatureProvider.cs
+++ src/CharonX.Core/Features/PesCloudFeatureProvider.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Abp.Application.Features;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 using Abp.Localization;
 using Abp.UI.Inputs;
+using CharonX.Authorization.Users;
+using CharonX.Entities;
+using CharonX.MultiTenancy;
 
 namespace CharonX.Features
 {
@@ -14,14 +21,19 @@ namespace CharonX.Features
         public static string SourceName = CharonXConsts.LocalizationSourceName;
 
         private readonly ILocalizationManager _localizationManager;
+        private readonly IRepository<CustomFeatureSetting> featureRepository;
 
-        public PesCloudFeatureProvider(ILocalizationManager localizationManager)
+        public PesCloudFeatureProvider(
+            IRepository<CustomFeatureSetting> featureRepository
+            )
         {
-            _localizationManager = localizationManager;
+            //_localizationManager = localizationManager;
+            this.featureRepository = featureRepository;
         }
 
         public override void SetFeatures(IFeatureDefinitionContext context)
         {
+#if true
             var smartSecurityFeature = context.Create(
                 SmartSecurityFeature,
                 displayName: new LocalizableString(SmartSecurityFeature, SourceName),
@@ -35,6 +47,26 @@ namespace CharonX.Features
                 defaultValue: "false",
                 inputType: new CheckboxInputType()
             );
+#endif
+            //additional features
+            var features = GetAllFeatures();
+            foreach (var feature in features)
+            {
+                context.Create(
+                    feature,
+                    displayName: new LocalizableString(feature, SourceName),
+                    defaultValue: "false",
+                    inputType: new CheckboxInputType());
+            }
+        }
+
+        private List<string> GetAllFeatures()
+        {
+            List<string> featureStringList;
+            var features = featureRepository.GetAllList();
+            featureStringList = features
+                .Select(feature => feature.Name).ToList();
+            return featureStringList;
         }
     }
 }
